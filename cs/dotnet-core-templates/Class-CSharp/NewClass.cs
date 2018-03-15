626e640f17b1aac17e7c790ca51b4956c68a9b80
Dan Walmsley <dan@walms.co.uk>
2018-03-15 21:44:27 +0000

    update templates

--- cs/dotnet-core-templates/Class-CSharp/NewClass.cs
+++ cs/dotnet-core-templates/Class-CSharp/NewClass.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AvalonStudioAppTemplate.Namespace
 {
-    interface Interface
+    class NewClass
     {
     }
 }
--- cs/dotnet-core-templates/Interface-CSharp/NewInterface.cs
+++ cs/dotnet-core-templates/Interface-CSharp/NewInterface.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AvalonStudioAppTemplate.Namespace
 {
-    class Class
+    interface NewInterface
     {
     }
 }
