9c750f0dd8beafc76aaa34043d850b62e87e9774
Dan Walmsley <dan@walms.co.uk>
2018-03-22 17:35:11 +0000

    Add a c++ class template.

---
+++ Cpp/Class/NewClass.h
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *  Description:
+ *
+ *       Author:
+ *
+ *******************************************************************************/
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/**
+ *  NewClass
+ */
+class NewClass
+{
+  public:
+    /**
+     * Instantiates a new instance of NewClass
+     */
+    NewClass ();
+    
+    virtual ~NewClass ();
+};
